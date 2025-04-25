f9e25b24ea7cff6153a1650bb6de1481c9b67104
Jared Rethman <jared.rethman@automattic.com>
2025-04-25 11:47:04 -0500

    added lempifyd to workspace
    fixes directory resolution for releases

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -7,3 +7,8 @@ export const SUPPORTED_OSS = ["aarch64-apple-darwin",/*  "x86_64-apple-darwin" *
  * The directory containing the daemon source code.
  */
 export const LEMPIFYD_DIR = `src-tauri/src/bin/lempifyd`;
+
+/**
+ * The directory containing the release binaries.
+ */
+export const RELEASE_DIR = `src-tauri/target`;
