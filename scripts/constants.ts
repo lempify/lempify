0cea8c18cfac1a51dd3795a3ac0b54e51dbf9090
Jared Rethman <jared.rethman@automattic.com>
2025-04-25 09:59:19 -0500

    finalized lempifyd scripts

---
+++ scripts/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Supported OS architectures.
+ */
+export const SUPPORTED_OSS = ["aarch64-apple-darwin",/*  "x86_64-apple-darwin" */];
+
+/**
+ * The directory containing the daemon source code.
+ */
+export const LEMPIFYD_DIR = `src-tauri/src/bin/lempifyd`;
