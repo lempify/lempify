1f78a1d3e47f14fa04f9a9b52a5b5c1d5b8a88cf
Jared Rethman <jared.rethman@automattic.com>
2025-05-01 10:44:29 -0500

    add `useLempifyd` hook
    facilitates service invokes to backend

--- src/App.tsx
+++ src/App.tsx
@@ -15,34 +15,35 @@ import Header from "./ui/Header";
 import Sidebar from "./ui/Sidebar";
 import Dashboard from "./ui/Dashboard";
 import Settings from "./ui/Settings";
-import { useEffect } from "react";
-import startPhp from "./services/startPhp";
+import { ServiceProvider } from "./context/ServiceContext";
+
+import useLempifyd from "./hooks/useLempifyd";
 
 const App = () => {
-  useEffect(() => {
-    startPhp();
-  }, []);
-
+  // Start Nginx, MySQL, PHP services
+  useLempifyd();
+  
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <div className="flex">
-          <Sidebar />
-          <div className="flex-1 flex flex-col ml-64">
-            <main className="p-10 bg-[var(--lempify-bg)] text-[var(--lempify-text)]">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/sites" element={<Sites />} />
-                <Route path="/settings" element={<Settings />} />
-              </Routes>
-            </main>
+    <ServiceProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <div className="flex">
+            <Sidebar />
+            <div className="flex-1 flex flex-col ml-64">
+              <main className="p-10 bg-[var(--lempify-bg)] text-[var(--lempify-text)]">
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/sites" element={<Sites />} />
+                  <Route path="/settings" element={<Settings />} />
+                </Routes>
+              </main>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ServiceProvider>
   );
 };
-
 
 export default App;
