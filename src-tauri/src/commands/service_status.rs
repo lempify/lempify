a53fc738fb90c34f6aac88d17099da36b63253d2
Jared Rethman <jared.rethman@automattic.com>
2025-05-08 08:36:33 -0500

    regex for service versions

--- src-tauri/src/commands/service_status.rs
+++ src-tauri/src/commands/service_status.rs
@@ -1,9 +1,28 @@
-use crate::helpers::service_utils::{
-    get_brew_formula, get_version, get_version_args,
-};
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+use crate::helpers::service_utils::{get_brew_formula, get_version, get_version_args};
 use crate::models::service::{ServiceStatus, ServiceType};
 
 use shared::utils::brew;
+
+/**
+ * This regex is used to extract the version from the service output.
+ * It matches the version in the following formats:
+ * - PHP 8.4.24
+ * - Ver 1.23.0
+ * - nginx/1.23.0
+ */
+static VERSION_REGEX: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"(?m)(?:PHP\s+|Ver\s+|nginx/)(?P<version>\d+(?:\.\d+)*)")
+        .expect("invalid regex")
+});
+
+fn extract_version(input: &str) -> Option<String> {
+    VERSION_REGEX
+        .captures(input)
+        .and_then(|caps| caps.name("version").map(|m| m.as_str().to_owned()))
+}
 
 #[tauri::command]
 pub async fn get_service_status(service: ServiceType) -> ServiceStatus {
@@ -16,12 +35,14 @@ pub async fn get_service_status(service: ServiceType) -> ServiceStatus {
         None
     };
 
-    let running = brew::is_service_running(bin);
-
-    ServiceStatus {
+    let service_status = ServiceStatus {
         name: bin.to_string(),
+        running: brew::is_service_running(bin),
+        version: version.as_deref().and_then(extract_version).or_else(|| Some("".to_string())),
         installed,
-        version,
-        running,
-    }
+    };
+
+    println!("Service status: {:?}", &service_status);
+
+    service_status
 }
