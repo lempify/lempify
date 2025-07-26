b0cbe9b9ba18ed7307f455daa6e72a9a43acf0a8
Jared Rethman <jared.rethman@automattic.com>
2025-07-25 21:42:37 -0500

    feat: a11y context

--- src/App.tsx
+++ src/App.tsx
@@ -17,39 +17,42 @@ import Sidebar from './ui/Sidebar';
 import Settings from './ui/Settings';
 import Site404 from './ui/Site404';
 import Page404 from './ui/Page404';
+import Background from './ui/Background';
+
 import { AppConfigProvider } from './context/AppConfigContext';
 import { LempifydProvider } from './context/LempifydContext';
-
-import Background from './ui/Background';
+import { A11yProvider } from './context/A11yContext';
 
 const App = () => {
   return (
     <AppConfigProvider>
       <LempifydProvider>
-        <Router>
-          <div className='h-screen grid grid-cols-[auto_1fr] grid-rows-[65px_1fr]'>
-            {/* Header - sticky */}
-            <Header />
-            {/* Sidebar - sticky */}
-            <Sidebar />
-            {/* Main content */}
-            <main className='overflow-y-auto bg-neutral-100 dark:bg-neutral-900 text-[var(--lempify-text)]'>
-              <Background />
-              <div className='p-10 relative'>
-                <Routes>
-                  <Route path='/' element={<></>} />
-                  <Route path='/sites' element={<Sites />} />
+        <A11yProvider>
+          <Router>
+            <div className='h-screen grid grid-cols-[auto_1fr] grid-rows-[65px_1fr]'>
+              {/* Header - sticky */}
+              <Header />
+              {/* Sidebar - sticky */}
+              <Sidebar />
+              {/* Main content */}
+              <main className='overflow-y-auto bg-neutral-100 dark:bg-neutral-900 text-[var(--lempify-text)]'>
+                <Background />
+                <div className='p-10 relative'>
+                  <Routes>
+                    <Route path='/' element={<></>} />
+                    <Route path='/sites' element={<Sites />} />
 
-                  <Route path='/sites/:domain' element={<Site />} />
-                  <Route path='/sites/*' element={<Site404 />} />
+                    <Route path='/sites/:domain' element={<Site />} />
+                    <Route path='/sites/*' element={<Site404 />} />
 
-                  <Route path='/settings' element={<Settings />} />
-                  <Route path='*' element={<Page404 />} />
-                </Routes>
-              </div>
-            </main>
-          </div>
-        </Router>
+                    <Route path='/settings' element={<Settings />} />
+                    <Route path='*' element={<Page404 />} />
+                  </Routes>
+                </div>
+              </main>
+            </div>
+          </Router>
+        </A11yProvider>
       </LempifydProvider>
     </AppConfigProvider>
   );
