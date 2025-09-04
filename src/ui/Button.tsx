fcbd3030e4a45aedc9eb4e496c123b545e5f87d1
Jared Rethman <jared.rethman@automattic.com>
2025-09-04 12:57:11 -0500

    feat: add blank button type

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -2,27 +2,31 @@ export default function Button({
   children,
   onClick,
   className = '',
-  size = 'sm',
+  size = '',
   disabled = false,
+  isRounded = false,
 }: {
   children: React.ReactNode;
   onClick: () => void;
   className?: string;
-  size?: 'sm' | 'xs' | 'md' | 'lg';
+  size?: '' | 'sm' | 'xs' | 'md' | 'lg';
   disabled?: boolean;
+  isRounded?: boolean;
 }) {
   return (
     <button
-      className={`disabled:opacity-50 disabled:cursor-not-allowed rounded-full ${
-        size === 'sm'
-          ? 'text-xs px-2 py-1'
-          : size === 'xs'
-            ? 'text-xs px-1.5 py-0.5'
-            : size === 'md'
-              ? 'text-sm px-3 py-2'
-              : size === 'lg'
-                ? 'text-lg px-4 py-3'
-                : 'text-xs px-2 py-1' // default
+      className={`disabled:opacity-50 disabled:cursor-not-allowed ${isRounded ? 'rounded-full' : ''} ${
+        size === ''
+          ? ''
+          : size === 'sm'
+            ? 'text-xs px-2 py-1'
+            : size === 'xs'
+              ? 'text-xs px-1.5 py-0.5'
+              : size === 'md'
+                ? 'text-sm px-3 py-2'
+                : size === 'lg'
+                  ? 'text-lg px-4 py-3'
+                  : 'text-xs px-2 py-1' // default
       } ${className}`}
       onClick={onClick}
       {...{ disabled }}
