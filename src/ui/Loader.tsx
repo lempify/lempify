19738cb343ed97e2ae379cedb11248743eaf8936
Jared Rethman <jaredrethman@gmail.com>
2025-09-04 13:02:59 -0500

    refactor: loader css

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -2,7 +2,11 @@ import { SvgSpinner } from './Svg';
 
 export default function Loader({ isVisible = false, size = 50 }: { isVisible?: boolean, size?: number }) {
   return isVisible ? (
-    <div className='absolute top-0 left-0 w-full h-full bg-neutral-200/50 dark:bg-neutral-800/50'>
+    <div className='
+      absolute top-0 left-0 w-full h-full 
+      bg-white/30 dark:bg-black/30 
+      shadow-inner-lg shadow-white/10 dark:shadow-black/10
+     '>
       <div
         className={`absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%]`}
       >
