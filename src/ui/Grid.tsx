eb7a0a2eb8cea6bd92e77d7c74c6dd150a5e42fd
Jared Rethman <jaredrethman@gmail.com>
2025-09-26 15:05:39 -0500

    fix: two column

--- src/ui/Grid.tsx
+++ src/ui/Grid.tsx
@@ -1,46 +1,56 @@
-const gridCss = `
-grid 
-grid-cols-1 
+const GRID_CSS = `
+  grid 
+  grid-cols-1 
 
-@lg:grid-cols-2 
+  @lg:grid-cols-2 
 
-@2xl:grid-cols-3
+  @2xl:grid-cols-3
 `;
 
-const gridItemCss = `
-    p-4
+const GRID_ITEM_CSS = `
+  p-4
 
-    border-t border-r-0 border-neutral-300 dark:border-neutral-700 
+  border-t border-r-0 border-neutral-300 dark:border-neutral-700 
 
-    @lg:border-r 
-    @lg:@max-2xl:[&:nth-child(2n)]:border-r-0
-    
-    @2xl:[&:nth-child(3n)]:border-r-0
+  @max-lg:empty:border-t-0 
+  @max-lg:empty:hidden 
 
-    relative 
+  @lg:border-r 
+  @lg:@max-2xl:[&:nth-child(2n)]:border-r-0 
+  
+  @2xl:[&:nth-child(3n)]:border-r-0 
+
+  relative 
 `;
 
 export function Grid({
   children,
   className = '',
-  maxCols = 3,
   childrenLength = 0,
 }: {
   children?: React.ReactNode;
   className?: string;
-  maxCols?: number;
   childrenLength?: number;
 }) {
+  const maxCols = 3;
+  const emptyItemsCount =
+    childrenLength % maxCols > 0 ? maxCols - (childrenLength % maxCols) : 0;
+  const shouldHideEmptyItems = emptyItemsCount === 2;
+
   return (
     <div className={`@container ${className}`}>
-      <ul className={gridCss}>
+      <ul className={GRID_CSS}>
         <>
           {children}
-          {childrenLength % maxCols > 0 && Array.from({ length: maxCols - (childrenLength % maxCols) }).map(
-            (_, index) => (
-              <GridItem key={index} />
-            )
-          )}
+          {emptyItemsCount > 0 &&
+            Array.from({ length: emptyItemsCount }).map((_, index) => (
+              <GridItem
+                key={index}
+                className={
+                  shouldHideEmptyItems ? '@lg:@max-2xl:empty:hidden' : ''
+                }
+              />
+            ))}
         </>
       </ul>
     </div>
@@ -54,5 +64,6 @@ export function GridItem({
   children?: React.ReactNode;
   className?: string;
 }) {
-  return <li className={`${gridItemCss} ${className}`}>{children}</li>;
+  const combinedClassName = `${GRID_ITEM_CSS} ${className}`;
+  return <li className={combinedClassName}>{children}</li>;
 }
